107ec07b08482b6c859a4af3b09da7d039d17a33
vinromarin <vinogradova.marina@gmail.com>
2019-12-04 23:24:28 +0100

    chapter1

--- go/tour-to-go/01-packages.go
+++ go/tour-to-go/01-packages.go
@@ -7,11 +7,14 @@ import (
   "math"
 )
 
+const Pi = 3.14
+
 func main() {
   fmt.Println("Pi = ", math.Pi) // name is exported if it begins with a capital letter - Pi
                                 // "unexported" names are not accessible from outside the package
-  fmt.Println("add:", add(42,13))
-  fmt.Println(swap("Hello", "World"))
+  fmt.Println("add:", add(42,13)) // -> 35
+  fmt.Println(swap("Hello", "World")) // -> World Hello
+  fmt.Println(returnNamedValues(2))   // -> 200 1400
 }
 
 func add(x, y int) int {
@@ -20,4 +23,10 @@ func add(x, y int) int {
 
 func swap(x, y string) (string, string) { // a func can return any number of arguments
   return y, x
+}
+
+func returnNamedValues(input int) (value1, value2 int) {
+  value1 = 100 * input
+  value2 = value1 * 7
+  return // "naked" return = return w/o args
 }
